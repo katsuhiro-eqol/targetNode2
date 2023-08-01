c9d7f85208e453ccdd8fcd36fc3ac66f9792ea53
katsuhiro_yamashita <katsuhiro.yamashita.sr01@gmail.com>
2023-08-01 11:21:41 +0900

    20230801

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  //apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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"
 });
 const openai = new OpenAIApi(configuration);
 
