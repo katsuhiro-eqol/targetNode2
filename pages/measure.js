cbc046151db90a5ed1b8d3d73e370ff78e46767f
katsuhiro_yamashita <katsuhiro.yamashita.sr01@gmail.com>
2023-09-10 13:16:36 +0900

    20230910

--- pages/measure.js
+++ pages/measure.js
@@ -15,14 +15,17 @@ export default function Measure() {
   const [wavFiles, setWavFiles] = useState([])
   const wavRef = useRef()
 
+
   const bucket_path = "gs://targetproject-394500.appspot.com/" //cloud storage bucket
   const silent = "https://firebasestorage.googleapis.com/v0/b/targetproject-394500.appspot.com/o/setto%2Fno_sound.mp3?alt=media&token=99787bd0-3edc-4f9a-9521-0b73ad65eb0a"
   const speech = "https://firebasestorage.googleapis.com/v0/b/targetproject-394500.appspot.com/o/setto%2F85602777dc2cbb2c4806cc5c5070b422.wav?alt=media&token=cce161c5-c0dd-468f-96a9-7fbf1660e434"
+  //wavRef.current = speech
+  const [play] = useSound(wavRef.current)
 
   const listFiles = () => {
+    let files = []
     const path = bucket_path + "setto"
     const listRef = ref(storage, path);
-    let files = []
     listAll(listRef)
     .then((res) => {
       res.items.forEach((itemRef) => {
@@ -34,31 +37,22 @@ export default function Measure() {
     });
   }
 
-  listFiles()
+  //listFiles()
 
   const selectFile = () => {
     const file = wavFiles[Math.floor(Math.random() * wavFiles.length)]
     return file
   }
 
-  const [play] = useSound(wavRef.current)
-
-  const speak = () => {
-    console.log("speak")
-    wavRef.current = speech
-    play()
-  }
-
   const dlFile = () => {
     const selected = selectFile()
-    console.log("selected:",selected)
     const path = bucket_path + "setto/" + selected
     const pathRef = ref(storage, path);
     getDownloadURL(pathRef)
       .then((url) => {
         wavRef.current = url
         console.log("wavRef:", wavRef.current)
-        play(wavRef.current)
+        //[play] = useSound(wavRef.current)
       })
       .catch((error) => {
         //エラー処理
@@ -69,9 +63,6 @@ export default function Measure() {
   async function onSubmit(event) {
     const now = new Date()
     const start = now.getTime()
-    setwavReady(false)
-    setPrompt("")
-    setResult("")
     event.preventDefault();
     try {
       const response = await fetch("/api/generate3", {
@@ -83,40 +74,15 @@ export default function Measure() {
       });
 
       const data = await response.json();
+      //console.log(data)
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-      setPrompt(data.prompt)
-      setResult(data.result);
       setUserInput("");
       //ここにwav処理
-      const currentWavPath = bucket_path + data.wav;//urlを返すようにaws flask側を変更
-      const currentRef = ref(storage, currentWavPath)
-      getDownloadURL(currentRef)
-      .then((url) => {
-        //wavRef.current = url
-        //console.log("wavUrl", wavRef.current)
-        setWavUrl(url);
-        //const [play] = useSound(wavRef.current);
-        //play()
-        async function resolveSample() {
-            //wavRef.current = url
-            wavRef.current = url
-            return wavRef;
-        }
-        resolveSample().then(value => {
-            console.log("wavUrl", value.current)
-            play(value.current); // => resolve!!
-        });
-      })
-      .catch((error) => {
-        //エラー処理
-      })
-
-      const response_time = timeMeasurement(start)
-      console.log(response_time)
-      console.log(data.openai_time)
-
+      wavRef.current = data.wav
+      //play()
+      console.log(wavRef.current)
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -124,34 +90,11 @@ export default function Measure() {
     }
   }
 
-  const timeMeasurement = (start) => {
-    const now = new Date()
-    const time = now.getTime()
-    const elapsed = time -start
-    return elapsed
-  }
-
-  function myPromise(url) {
-    return new Promise(function(resolve) {
-      resolve(wavRef.current = url)
-    })
-  }
-  async function myAsync() {
-    await myPromise(wavRef.current);
-    play(wavRef.current)
-  }
-
-
-
-  //const [play] = useSound(wavRef.current);
-  //const [play] = useSound(wavUrl);
-
-  /*
+/*
   useEffect(() => {
-    //console.log(wavFiles)
-  }, [wavFiles])
-  */
-
+    console.log("UE:", wavUrl)
+  }, [wavUrl])
+*/
   return (
     <div>
       <Head>
@@ -170,13 +113,10 @@ export default function Measure() {
           />
           <input disabled={false} type="submit" value="伝える" />
         </form>
-        <div className={styles.result}>{prompt}</div>
-        <div className={styles.result}>{result}</div>
         <br/>
-        <button onClick={() => speak()}>speak!!</button>
-        <button onClick={dlFile}>dlFile!!</button>
-        <br/>
-        <div className={styles.url}>{wavUrl}</div>
+        <button onClick={listFiles}>listFile</button>
+        <button onClick={dlFile}>DL only</button>
+        <button onClick={() => play(wavRef.current)}>speak</button>
       </main>
     </div>
   );
